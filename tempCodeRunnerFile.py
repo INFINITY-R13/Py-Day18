0af8879da6b87a13fde17a57a53ee518754afa06
Raghuraj Singh <hannuseven@gmail.com>
2024-09-03 19:36:45 +0530

    Draw a square

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+for _ in range(4):
+    the_turtle.forward(100)
+    the_turtle.left(90)
---
+++ turtle-graphics.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+
+the_turtle = Turtle()
+the_turtle.shape("turtle")
+the_turtle.color("pink")
+
+# Draw a square
+for _ in range(4):
+    the_turtle.forward(100)
+    the_turtle.left(90)
+
+
+
+
+
+
+
+
+
+my_screen = Screen()
+my_screen.exitonclick()
