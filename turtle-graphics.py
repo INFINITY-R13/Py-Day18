be360fde6d0061f513319e58bac37a32551917cf
Raghuraj Singh <hannuseven@gmail.com>
2024-09-03 19:37:23 +0530

    Draw a dashed line

--- turtle-graphics.py
+++ turtle-graphics.py
@@ -5,9 +5,16 @@ the_turtle.shape("turtle")
 the_turtle.color("pink")
 
 # Draw a square
-for _ in range(4):
-    the_turtle.forward(100)
-    the_turtle.left(90)
+# for _ in range(4):
+#     the_turtle.forward(100)
+#     the_turtle.left(90)
+
+# Draw a dashed line
+for _ in range(15):
+    the_turtle.forward(10)
+    the_turtle.penup()
+    the_turtle.forward(10)
+    the_turtle.pendown()
 
 
 
